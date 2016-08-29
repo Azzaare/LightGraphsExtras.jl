07ddebcd95aa4dc871c18030b8aaeddc7a2f0794
Azzaare <jeanfrancois.baffier@gmail.com>
2016-08-29 16:22:10 +0900

    ALgorithms for Network Interdiction (flow)

--- src/LightGraphsExtras.jl
+++ src/LightGraphsExtras.jl
@@ -4,10 +4,12 @@ export Matching
 export CombinatorialAdjacencies
 export Datasets
 export Community
+export Interdiction
 
 include("matching/matching.jl")
 include("spectral/spectral.jl")
 include("datasets/Datasets.jl")
 include("community/detection.jl")
+include("interdiction/Interdiction.jl")
 
 end # module
